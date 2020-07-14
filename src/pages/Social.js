4cb26dce1b5382bd91e469b9dc2a8f6ecc45c5db
gabrielferr26 <tecpt19@gmail.com>
2020-07-14 11:25:40 +0100

    changestyle

--- src/pages/Social.js
+++ src/pages/Social.js
@@ -13,7 +13,7 @@ import axios from 'axios'
 import Navbar from '../components/Navbar/Navbar'
 import ListaDevelopers from '../components/Lists/ListaDevelopers'
 import CardDevelopers from '../components/Cards/CardsTotalUsers'
-import { Button, Card, CardGroup } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import CardSkills from '../components/Cards/CardTotalSkills'
 import CardInteresses from '../components/Cards/CardTotalInteresses'
 import CardProjetos from '../components/Cards/CardTotalProjetos'
@@ -28,7 +28,7 @@ export default class Social extends React.Component {
         return (
             <div>
                 <Navbar />
-                <main>
+                <main style={{ marginRight: 60 }}>
                     <h5 style={{ fontFamily: 'BoldG', fontSize: 24, marginLeft: 15, marginTop: 70 }}>Social</h5>
                     <div>
                         <nav aria-label="breadcrumb">
@@ -38,13 +38,11 @@ export default class Social extends React.Component {
                             </ol>
                         </nav>
                     </div>
-                    <div style={{ marginTop: -22 }} className="row ml-1 mr-4">
-                        <CardGroup>
-                            <CardDevelopers />
-                            <CardSkills />
-                            <CardInteresses />
-                            <CardProjetos />
-                        </CardGroup>
+                    <div style={{ marginTop: -22 }} className="row ml-1">
+                        <CardDevelopers />
+                        <CardSkills />
+                        <CardInteresses />
+                        <CardProjetos />
                         <ListaDevelopers />
                         <div className="col-xl-6 mt-3">
                             <UsersDisponiveis />
